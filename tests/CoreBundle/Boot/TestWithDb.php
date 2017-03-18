f0345bd872e6dd47162790112eed8f58ed40b825
Arash Afshar <arash.afsharpayam@mail.com>
2017-03-18 23:51:03 +0100

    Some php unit tests fixed

--- tests/CoreBundle/Boot/TestWithDb.php
+++ tests/CoreBundle/Boot/TestWithDb.php
@@ -1,20 +1,11 @@
 <?php
 namespace Tests\CoreBundle\Boot;
 
-use BackendBundle\Service\Db\AdministratorService;
-use BackendBundle\Service\Db\ReorderService;
-use CoreBundle\Service\Db\CategoryService;
-use CoreBundle\Service\Db\CustomerService;
-use CoreBundle\Service\Db\OrderService;
-use CoreBundle\Service\Db\ProductService;
-use CoreBundle\Service\Security\SecurityService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\SchemaTool;
-use FrontendBundle\Service\Db\RegistrationService;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class TestWithDb extends KernelTestCase
+class TestWithDb extends TestServices
 {
 
     /**
@@ -22,9 +13,6 @@ class TestWithDb extends KernelTestCase
      */
     protected $em;
 
-    /**
-     * @var ContainerInterface
-     */
     protected $container;
 
     /**
@@ -34,54 +22,9 @@ class TestWithDb extends KernelTestCase
 
     private $metadata;
 
-    protected function administratorService(): AdministratorService
+    function getContainer(): ContainerInterface
     {
-        return $this->container->get('backend.service.administrator');
-    }
-
-    protected function backendSecurityService(): SecurityService
-    {
-        return $this->container->get('backend.service.db.security');
-    }
-
-    protected function categoryService(): CategoryService
-    {
-        return $this->container->get('service.category');
-    }
-
-    protected function productService(): ProductService
-    {
-        return $this->container->get('service.product');
-    }
-
-    protected function backendProductService(): \BackendBundle\Service\Db\ProductService
-    {
-        return $this->container->get('backend.service.product');
-    }
-
-    protected function customerService(): CustomerService
-    {
-        return $this->container->get('frontend.service.db.customer');
-    }
-
-    protected function registrationService(): RegistrationService
-    {
-        return $this->container->get('frontend.service.db.registration');
-    }
-
-    protected function orderService(): OrderService
-    {
-        return $this->container->get('service.order');
-    }
-
-    protected function frontendOrderService(): \FrontendBundle\Service\Db\OrderService
-    {
-        return $this->container->get('frontend.service.order');
-    }
-
-    protected function backendReorderService(): ReorderService
-    {
-        return $this->container->get('backend.service.reorder');
+        return $this->container;
     }
 
     protected function setUp()
